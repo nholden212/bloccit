167c4eed0689f10988a931ce378fd9b86cb90c72
Nick Holden <nholden212@gmail.com>
2018-09-16 12:23:49 -0700

    assignment 2 complete

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const marco = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
 
@@ -15,7 +16,7 @@ describe("routes : static", () => {
 
   describe("GET /marco", () => {
     it("should return status code 200 and the body should contain 'polo'", (done) => {
-      request.get(base, (err, res, body) => {
+      request.get(marco, (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(body).toBe("polo");
         done();
