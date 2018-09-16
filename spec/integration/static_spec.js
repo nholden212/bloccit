0d1add78b895868f2e3847b8196d4aeed6fc99b4
Nick Holden <nholden212@gmail.com>
2018-09-16 12:10:25 -0700

    Getting mentor help

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -3,6 +3,7 @@ const server = require("../../src/server");
 const base = "http://localhost:3000/";
 
 describe("routes : static", () => {
+
   describe("GET /", () => {
     it("should return status code 200", (done) => {
       request.get(base, (err, res, body) => {
@@ -11,4 +12,15 @@ describe("routes : static", () => {
       });
     });
   });
+
+  describe("GET /marco", () => {
+    it("should return status code 200 and the body should contain 'polo'", (done) => {
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toBe("polo");
+        done();
+      });
+    });
+  });
+
 });
