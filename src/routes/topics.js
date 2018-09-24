a0276bc34ba1fc56dfd8b7b30418411ea8ba2b64
Nick Holden <nholden212@gmail.com>
2018-09-23 20:29:25 -0700

    checkpoint 9 revised final

--- src/routes/topics.js
+++ src/routes/topics.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const topicController = require("../controllers/topicController");
 
 router.get("/topics", topicController.index);
-router.get("/topcs/new", topicController.new);
+router.get("/topics/new", topicController.new);
 router.post("/topics/create", topicController.create);
 router.get("/topics/:id", topicController.show);
 router.post("/topics/:id/destroy", topicController.destroy);
