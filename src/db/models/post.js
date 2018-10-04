01547f8161bfc3b2db1e988a57236419e4c541b6
Nick Holden <nholden212@gmail.com>
2018-10-04 13:18:22 -0700

    Assignment 15 complete

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -46,6 +46,12 @@ module.exports = (sequelize, DataTypes) => {
         postId: post.id
       });
     });
+    Post.afterCreate((post, callback) => {
+      return models.Vote.create({
+        userId: post.userId,
+        postId: post.id
+      });
+    });
   };
   Post.prototype.isOwner = function() {
     return this.userId === this.foreignKey;
